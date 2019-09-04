4c36d1f51249f445c2efe29b41843195ad4e848e
jvalverde2018016 <jvalverde-2018016@kinal.edu.gt>
2019-09-04 08:28:45 -0600

    Creacion de la DB

---
+++ DBEasyCode.sql
@@ -0,0 +1,17 @@
+create database DBEasyCode;
+use DBEasyCode;
+
+create table Estudiantes (
+	codigoEstudiante int not null,
+	nombre varchar (50),
+    apellido varchar (50),
+    edad varchar (50),
+    fechaDeNaciemto varchar (50),
+    carrera varchar (20),
+    codigoTecnico varchar (15),
+    codigoAcademico varchar (15),
+    primary key PK_codigoEstudiante (codigoEstudiante)
+);
+
+Select *from Estudiantes;
+
