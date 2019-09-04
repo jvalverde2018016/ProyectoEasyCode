f5535b3611e0d6c8bb9573bb84c5d7dadedd9020
bmartinez2015568 <bmartinez-2015568@kinal.edu.gt>
2019-09-04 11:34:21 -0600

    Clase Principal y Menu Creado

--- MavenCRUDEasyCode/src/main/java/org/easycode/sistema/Principal.java
+++ MavenCRUDEasyCode/src/main/java/org/easycode/sistema/Principal.java
@@ -6,5 +6,19 @@ package org.easycode.sistema;
  * @author programacion
  */
 public class Principal {
-    
+    public static void main(String[] args) {
+        System.out.println("¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦"); 
+        System.out.println("*****  -Menu de Opciones-  *****");   			         System.out.println("¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦"); 
+        System.out.println("---------------------------------------------");
+        System.out.println("-1.> Mostrar estudiantes por Código");
+        System.out.println("-2.> Agregar estudiante al SGBD ---");
+        System.out.println("-3.> Actualizar una tupla -------------");
+        System.out.println("-4.> Eliminar una tupla ---------------");
+        System.out.println("-5.> Listar Personas -------------------");
+        System.out.println("-6.> Salir --------------------------------");
+        System.out.println("--------------------------------------------");
+        System.out.println("Ingrese una opcion a realizar...");
+        op = sc.nextInt();
+
+    }
 }
